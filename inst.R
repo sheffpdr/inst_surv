ce3fe1c0cc379bd1ddfb4990fea62ffadca8cd8e
Paul Richards <sheff.pdr@gmail.com>
2016-03-07 13:34:43 +0000

    ideas

---
+++ inst.R
@@ -0,0 +1,9 @@
+# Code to perform instrumental variables analysis comparing surgery with ``PET''
+
+# Number of strategies; 1. unrestricted
+#                       2. unrestricted, adjust for stage differences
+#                       3. unrestricted, fully adjusted
+#                       4. restricted based on complete data
+#                       5. restricted complete, fully adjusted
+#                       6. restricted after multiple imputation, fully adjusted
+
